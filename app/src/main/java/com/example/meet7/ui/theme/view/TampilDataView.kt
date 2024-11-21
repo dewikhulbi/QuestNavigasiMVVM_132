f979fe22f93547d01590e6115b2ee737890240cd
dewikhulbi <dewikhlb20@gmail.com>
2024-11-21 15:31:57 +0700

    menambahkan tampil data untuk nim

---
+++ app/src/main/java/com/example/meet7/ui/theme/view/TampilDataView.kt
@@ -0,0 +1,53 @@
+package com.example.meet7.ui.theme.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.meet7.model.Datasiswa
+
+@Composable
+fun TampilDataView(
+    uiState: Datasiswa,
+    onBackButton: () -> Unit
+){
+    Column (
+        modifier = Modifier.fillMaxSize().padding(16.dp))
+        {
+         TampilData(param = "Nama", argu = uiState.nama)
+            TampilData(param = "Nim", argu = uiState.Nim)
+            TampilData(param = "email", argu = uiState.email)
+            TampilData(param = "Gender", argu = uiState.gender)
+            TampilData(param = "No Telp", argu = uiState.notelepon)
+            TampilData(param = "Alamat", argu = uiState.alamat)
+            Button(onClick = onBackButton) {
+                Text(text= "Kembali")
+            }
+        }
+}
+
+@Composable
+fun TampilData( param:String, argu:String){
+    Column (
+        modifier = Modifier.padding(16.dp)
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(text = param,
+                modifier = Modifier.weight(0.8f))
+            Text(text = ": ",
+                modifier = Modifier.weight(0.2f))
+            Text(text = argu,
+                modifier = Modifier.weight(2f))
+        }
+    }
+}
