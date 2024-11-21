4774ed9f70ae839eabe13fc2c5184d912d577f93
dewikhulbi <dewikhlb20@gmail.com>
2024-11-21 15:44:32 +0700

    membuat kelas data siswa untuk menyimpan data

---
+++ app/src/main/java/com/example/meet7/model/Datasiswa.kt
@@ -0,0 +1,10 @@
+package com.example.meet7.model
+
+data class Datasiswa(
+    val nama:String ="",
+    val Nim:String="",
+    val email: String="",
+    val alamat:String ="",
+    val gender:String ="",
+    val notelepon: String=""
+)
